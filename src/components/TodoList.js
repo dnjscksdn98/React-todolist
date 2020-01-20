b5aac24e57efec7c87653d337ed9d840e0dcd6b1
Alex <dnjscksdn98@gmail.com>
2020-01-20 22:32:19 +0900

    Add data by state context

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import TodoItem from "./TodoItem";
+import { useTodoState } from "../TodoContext";
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -10,12 +11,18 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+  const todos = useTodoState(); // bring the todos by using custom hook
+
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done />
-      <TodoItem text="컴포넌트 스타일링 하기" done />
-      <TodoItem text="Context 만들기" />
-      <TodoItem text="기능 구현하기" />
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
